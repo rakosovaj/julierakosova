74194aa34c613b12cd573b881a8d9d676fa8303d
kunesv <kunesv@sprinters.cz>
2018-11-01 22:26:53 +0100

    map url

--- docs/js/link.js
+++ docs/js/link.js
@@ -1,6 +1,6 @@
 document.querySelector('#street').addEventListener('click', function (e) {
     if (navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|android)/i)) {
-        location.href = 'https://maps.apple.com/?ll=50.0992825,14.3837428'
+        location.href = 'https://maps.apple.com/?ll=50.0992825,14.3837428&address=Velvarská+13,Praha+6'
     } else {
         location.href = 'https://mapy.cz/s/3aTfw';
     }
