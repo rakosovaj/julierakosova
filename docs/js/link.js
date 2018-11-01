47b12144c8db2085f619e20fafeb7dde13c8f6ca
kunesv <kunesv@sprinters.cz>
2018-11-01 22:22:48 +0100

    map url

--- docs/js/link.js
+++ docs/js/link.js
@@ -1,5 +1,9 @@
+document.querySelector('#street').addEventListener('click', function (e) {
+    if (navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|android)/i)) {
+        location.href = 'https://maps.apple.com/?ll=50.0992825,14.3837428'
+    } else {
+        location.href = 'https://mapy.cz/s/3aTfw';
+    }
 
-document.querySelector('#street').addEventListener('click', function () {
-    window.alert('User Agent: ' + navigator.userAgent)
-    //location.href = "geo:50.0992825,14.3837428";
+    e.preventDefault();
 });
